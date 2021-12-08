6bccc502a26ac026f1ba0d0ab974ccca41b58ff2
Jay Mohabir <jayrainman1@gmail.com>
2021-12-08 09:05:12 -0800

    adding py func files

---
+++ Day1/py_func_ex2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+def my_func(x, y, z=20)
+    return x + y + z
+
+
+my_list = [22, 17,19]
+my_dict = {"x": 13, "y": 22, "z": 1}
+
+print()
+return_val = my_func(10, 20, 30)
+print(f"Calling with three positional args: {return_val}")
+
+
